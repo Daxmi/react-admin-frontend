7a227d4643ebe818114f17ecd6c8e4dbb33f4159
David Ogbonnaya <ndavidogbonnaya@gmail.com>
2021-12-09 18:43:58 +0000

    added my branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div>Hi Maxi, I am here with you</div>;
+  return <div>Hi Maxi, I am here with you. Hello Sule, I am with you also. Btw, my name is Mazi, not Maxi. Thanks.</div>;
 }
 
 export default App;
